ffcdd59587073947e4e229a9b3d370ff4f11f85c
builden <billdeng@qq.com>
2015-09-23 13:59:15 +0800

    add moment test

--- moment/moment.js
+++ moment/moment.js
@@ -42,19 +42,26 @@ console.log(today.toDate());
 console.log(today.valueOf()); // GMT
 
 // is the same day
+//  support year, month, week, day, hour, minute, second
 console.log(today.isSame('2015-08-31 00:22:33', 'day'));
+console.log('isSame week: ', today.isSame('2015-09-05 00:22:33', 'week'));
 
 // start of day
 // will change origin datatime，if don't want, please clone a new instance
 console.log(fmt(today.clone().startOf('day'))); // YYYY-MM-DD 00:00:00:000
+console.log('startOf week: ' + fmt(today.clone().startOf('week'))); // YYYY-MM-DD 00:00:00:000
+console.log('startOf week: ' + fmt(today.clone().startOf('week').add(4, 'day'))); // YYYY-MM-DD 00:00:00:000
 
 // end of day
 console.log(fmt(today.clone().endOf('day'))); // YYYY-MM-DD 23:59:59:999
+console.log('endOf week: ' + fmt(today.clone().endOf('week'))); // YYYY-MM-DD 00:00:00:000
+console.log('endOf week: ' + fmt(today.clone().endOf('week').add(4, 'day'))); // YYYY-MM-DD 00:00:00:000
 
 // diff
 var a = moment('2015-09-01 10:00:01');
 var b = moment('2015-09-02 10:00:00');
 console.log('diff ' + b.diff(a, 'days'));  // 超过24小时才算一天, return 0
+console.log('diff second ' + b.diff(a, 'seconds'));  // b相对于a的时间，b比时间a时间晚则大于0
 var aStart = a.clone().startOf('day')
 var bStart = b.clone().startOf('day');
 console.log('diff ' + bStart.diff(aStart, 'day')); // return 1
@@ -79,3 +86,16 @@ console.log(fmt(today.clone().subtract(1, 'hours')));
 function fmt(day) {
   return day.format('YYYY-MM-DD HH:mm:ss.SSS');
 }
+
+// isSameWeek, 默认是从周日0点为分界线
+//  offday - [0..6] 从周日到周六
+//  offhour - [0..23] 从0点到23点
+function isSameWeek(a, b, offday, offHour) {
+  a = a.clone().subtract(offday || 0, 'day').subtract(offHour || 0, 'hour');
+  b = b.clone().subtract(offday || 0, 'day').subtract(offHour || 0, 'hour');
+  return a.isSame(b, 'week');
+};
+
+var a = moment('2015-09-03 00:59:59.999');
+var b = moment('2015-09-03 01:00:00.000');
+console.log('isSameWeek:' + isSameWeek(a, b, 4, 1));
