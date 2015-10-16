5f67ffd12249b7014023cc361bd218e1b91fefe2
builden <billdeng@qq.com>
2015-10-16 10:43:04 +0800

    add async-es7 test suit

--- es6-learn/async-es7.js
+++ es6-learn/async-es7.js
@@ -1,18 +1,20 @@
 import {promisify} from 'node-promise-es6';
 import fs from 'fs';
+import assert from 'assert';
 
-function timeout() {
+function timeout(ms = 1000) {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, 1000);
+    setTimeout(() => resolve(ms), ms);
   });
 }
 
-async function doSomething() {
+async function doSomething(ms) {
   console.log('doSomething beg');
+  await timeout(ms);
+  console.log('doSomething timeout');
   await promisify(fs.readFile)(__dirname + '/jsconfig.json');
   console.log('doSomething end');
-  await timeout();
-  console.log('doSomething timeout');
+  return ms;
 }
 
 async function func() {
@@ -20,11 +22,34 @@ async function func() {
   await timeout();
   console.log('after timeout');
 
-  await doSomething();
-  await doSomething();
+  // 并行 await* promises <=> await Promise.all(promises)
+  var [x, y] = await* [doSomething(1000), doSomething(2000)];
+  assert.equal(x, 1000);
+  assert.equal(y, 2000);
   console.log('func doSomething end');
   var ctx = await promisify(fs.readFile)(__dirname + '/jsconfig.json');
   console.log('func readFile end');
 }
 
 func();
+
+function callbackFn(x, y, cb) {
+  return cb(null, x, y);
+}
+// 返回{returnX: 1, returnY: 2};
+var cbf = promisify(callbackFn, ['returnX', 'returnY']);
+
+// 返回 [1, 2]
+var cbf2 = promisify(callbackFn);
+
+async function test() {
+  const {returnX, returnY} = await cbf(1, 2);
+  assert.equal(returnX, 1);
+  assert.equal(returnY, 2);
+
+  const [x, y] = await cbf2(1, 2);
+  assert.equal(x, 1);
+  assert.equal(y, 2);
+}
+
+test();
